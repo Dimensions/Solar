c0a1c75ae161a486b6080e4226d67756cba4bcfa
Jαdon Fowler <ph4sesaber@gmail.com>
2015-03-23 20:14:23 -0700

    Add back Location

---
+++ src/net/dimensions/solar/position/Location.java
@@ -0,0 +1,51 @@
+package net.dimensions.solar.position;
+
+import net.dimensions.solar.world.World;
+
+public class Location {
+
+	public World w;
+	public double x = 0;
+	public double y = 0;
+	public double z = 0;
+
+	public Location(World w, double x, double y, double z) {
+		this.w = w;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public World getWorld(){
+		return w;
+	}
+	
+	public void setWorld(World w){
+		this.w = w;
+	}
+	
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+	
+}
