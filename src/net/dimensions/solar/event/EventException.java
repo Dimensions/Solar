25091b5b8b42668a50f8c38c00407106c7bd6371
phase <ph4sesaber@gmail.com>
2015-03-24 07:42:36 -0700

    Exception for events

---
+++ src/net/dimensions/solar/event/EventException.java
@@ -0,0 +1,17 @@
+package net.dimensions.solar.event;
+
+public class EventException extends Exception{
+
+    public EventException(String message) {
+        super(message);
+    }
+
+    public EventException(Throwable cause) {
+        super(cause);
+    }
+
+    public EventException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
