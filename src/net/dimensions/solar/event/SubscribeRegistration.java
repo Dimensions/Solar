0a84cfe4f1b4a110e5c0dfc007b2b1f261c5da3a
phase <ph4sesaber@gmail.com>
2015-03-24 07:49:21 -0700

    Registration for Subscribers

---
+++ src/net/dimensions/solar/event/SubscribeRegistration.java
@@ -0,0 +1,27 @@
+package net.dimensions.solar.event;
+
+public class SubscribeRegistration{
+
+    private final EventExecutor executor;
+    private final Order order;
+    private final Object owner;
+
+    public SubscribeRegistration(EventExecutor e, Order o, Object ow){
+        executor = e;
+        order = o;
+        owner = ow;
+    }
+
+    public EventExecutor getExecutor(){
+        return executor;
+    }
+
+    public Order getOrder(){
+        return order;
+    }
+
+    public Object getOwner(){
+        return owner;
+    }
+
+}
