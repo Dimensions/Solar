26df2b1659987aa5338fb7adbb3d0d514d93d314
phase <ph4sesaber@gmail.com>
2015-03-24 00:44:18 -0700

    Whitespace, Fire, and Death

--- src/net/dimensions/solar/entity/Entity.java
+++ src/net/dimensions/solar/entity/Entity.java
@@ -5,30 +5,39 @@ import net.dimensions.solar.position.Location;
 import net.dimensions.solar.position.Orientation;		
 import net.dimensions.solar.position.Vector;		
 		
-public interface Entity {		
-			
-	public int getEntityId();		
-			
-	public Location getLocation();		
-			
-	public void setLocation(Location p);		
-			
-	public Orientation getOrientation();		
-			
-	public void setOrientation(Orientation o);		
-			
-	public Vector getVelocity();		
-			
-	public void setVelocity(Vector v);		
-			
-	public boolean isOnGround();		
-			
-	public void teleport(Location l);		
-			
-	public void teleport(Location l, Orientation o);		
-			
-	public void teleport(Entity e);		
-			
-	public List<Entity> getNearbyEntities(double x, double y, double z);		
-			
+public interface Entity{
+
+    public int getEntityId();
+
+    public Location getLocation();
+
+    public void setLocation(Location p);
+
+    public Orientation getOrientation();
+
+    public void setOrientation(Orientation o);
+
+    public Vector getVelocity();
+
+    public void setVelocity(Vector v);
+
+    public boolean isOnGround();
+
+    public void teleport(Location l);
+
+    public void teleport(Location l, Orientation o);
+
+    public void teleport(Entity e);
+
+    public List<Entity> getNearbyEntities(double x, double y, double z);
+
+    public int getFireTicks();
+
+    public void setFireTicks(int i);
+
+    /**
+     * Remove entity
+     */
+    public void kill();
+
 }
