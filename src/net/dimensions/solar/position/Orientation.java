bdb712193f0e6243ac32c8211b94333768ef73c6
Jαdon Fowler <ph4sesaber@gmail.com>
2015-03-23 20:15:54 -0700

    Create Orientation.java

---
+++ src/net/dimensions/solar/position/Orientation.java
@@ -0,0 +1,21 @@
+package net.dimensions.solar.position;
+
+public class Orientation {
+
+	private float yaw;
+	private float pitch;
+
+	public Orientation(float yaw, float pitch) {
+		this.yaw = yaw % 360;
+		this.pitch = pitch % 360;
+	}
+
+	public float getYaw() {
+		return yaw;
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+
+}
