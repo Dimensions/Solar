df421ce56efafcf0e035473d96d34c7e4daa60f6
phase <ph4sesaber@gmail.com>
2015-03-24 07:37:35 -0700

    Start on Event class

---
+++ src/net/dimensions/solar/event/Event.java
@@ -0,0 +1,15 @@
+package net.dimensions.solar.event;
+
+public abstract class Event{
+
+    protected boolean cancelled = false;
+
+    protected void setCancelled(boolean b){
+        cancelled = b;
+    }
+
+    public boolean isCancelled(){
+        return cancelled;
+    }
+
+}
