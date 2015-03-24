e716f5d7b505bf267ba54e8b6c1ac0498e9c3540
Jαdon Fowler <ph4sesaber@gmail.com>
2015-03-23 20:09:34 -0700

    Update and rename EntityPlayer.java to Player.java

--- src/net/dimensions/solar/entity/EntityPlayer.java
+++
@@ -1,6 +0,0 @@
-package net.dimensions.solar.entity;
-
-public interface EntityPlayer extends Entity
-{
-    String getDisplayName();
-}
---
+++ src/net/dimensions/solar/entity/Player.java
@@ -0,0 +1,7 @@
+package net.dimensions.solar.entity;
+
+public interface Player extends Entity{
+
+    public String getDisplayName();
+
+}
