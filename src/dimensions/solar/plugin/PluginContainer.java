7dfdd7b3276b08773e5db3697e0467acf70ed1ed
phase <ph4sesaber@gmail.com>
2015-03-29 19:35:23 -0700

    Fix ArrayLists

--- src/dimensions/solar/plugin/PluginContainer.java
+++ src/dimensions/solar/plugin/PluginContainer.java
@@ -11,6 +11,6 @@ public class PluginContainer
     public String pluginId;
     public String name = pluginId;
     public String version = "1.0.0";
-    public List<String> dependencies = new ArrayList<>();
-    public List<CommandContainer> commands = new ArrayList<>();
+    public List<String> dependencies = new ArrayList<String>();
+    public List<CommandContainer> commands = new ArrayList<String>();
 }
