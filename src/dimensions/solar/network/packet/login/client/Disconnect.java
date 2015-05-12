832330a507d0424d5d0e531d0af295b48ef73ae4
chibill <chibill110@gmail.com>
2015-05-12 15:50:30 -0500

    Fixing formating slowly

--- src/dimensions/solar/network/packet/login/client/Disconnect.java
+++ src/dimensions/solar/network/packet/login/client/Disconnect.java
@@ -1,7 +1,10 @@
 package dimensions.solar.network.packet.login.client;
 
 import dimensions.solar.network.packet.Packet;
+import dimensions.solar.network.packet.PacketDirection;
 
 public interface Disconnect extends Packet {
-
+    public default PacketDirection getDirection(){
+	return PacketDirection.SERVER_BOUND;
+    }
 }
--- src/dimensions/solar/network/packet/login/server/EncryptionResponse.java
+++ src/dimensions/solar/network/packet/login/server/EncryptionResponse.java
@@ -1,7 +1,10 @@
 package dimensions.solar.network.packet.login.server;
 
 import dimensions.solar.network.packet.Packet;
+import dimensions.solar.network.packet.PacketDirection;
 
 public interface EncryptionResponse extends Packet {
-
+    public default PacketDirection getDirection(){
+	return PacketDirection.CLIENT_BOUND;
+    }
 }
